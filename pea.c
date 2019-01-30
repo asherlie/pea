baf067a2aa13c8b954ef918b889e25ab0bb43b26
asherlie <lieber.asher@gmail.com>
2019-01-30 18:29:28 -0500

    sigqueues are now used to send requests

--- pea.c
+++ pea.c
@@ -12,7 +12,23 @@
 #include <unistd.h>
 #include <sys/stat.h> // umask
 
+#include <sys/socket.h>
+#include <signal.h>
+
 #include <syslog.h>
+
+#define LIST_PET   0
+#define SIGN_PET   1
+#define CREATE_PET 2
+
+// borrowed from github
+void signal_handler(int signum, siginfo_t* info, void* extra){
+      int operation = info->si_value.sival_int;
+      FILE* fp = fopen("/home/asher/boybo", "a");
+      fprintf(fp, "sig num: %i, received int: %i\n", signum,  operation);
+      fclose(fp);
+      /*return (operation > 2 || operation < 0) operation;*/
+}
 
 int pea_daem(){
       pid_t pid, sid;
@@ -39,6 +55,19 @@ int pea_daem(){
 
       /* we're now a ~~daemon~~ */
 
+      // setting up a socket to accept connections
+      int sock = socket(AF_INET, SOCK_STREAM, 0);
+      /* signals should be required to get attention before any data is sent through sockets
+       * they will be sent using sigqueue with int macros LIST_PET, SIGN_PET, CREATE_PET
+       */
+      struct sigaction action;
+      action.sa_flags = SA_SIGINFO;
+      action.sa_sigaction = &signal_handler;
+      /*accept();*/
+      if(sigaction(SIGHUP, &action, NULL) == -1){
+            syslog(LOG_ERR, "sigaction failed\n");
+            exit(EXIT_FAILURE);
+      }
       while(1){
             usleep(1000);
       }
--- pea_cli.c
+++ pea_cli.c
@@ -1,5 +1,23 @@
 #include <signal.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define LIST_PET   0
+#define SIGN_PET   1
+#define CREATE_PET 2
+
+int send_sig(pid_t pid, int pet_opt){
+      union sigval val;
+      val.sival_int = pet_opt;
+      /*val.sival_ptr = NULL;*/
+      return sigqueue(pid, SIGHUP, val) == 0;
+}
+
+_Bool sign_petition(pid_t pid){
+      /*return send_sig(pid, SIGN_PET);*/
+      return send_sig(pid, 69);
+}
 
 /*
  * usage:
@@ -17,8 +35,15 @@ int main(int argc, char** argv){
       }
       // sign and create both require 2 args
       if(argc < 3)return 1;
+      char uname[10];
+      uname[9] = 0;
+      getlogin_r(uname, 9);
       // sign petition
       if(*argv[1] == 's'){
+            // TODO: use my strtoi and make this safer
+            // bound checking, etc.
+            pid_t pid = atoi(argv[2]); 
+            sign_petition(pid);
             return 0;
       }
       // create petition
